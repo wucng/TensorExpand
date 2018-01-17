dffbefc62b2d0f8c5537c241d102e2c9f262a810
fengzhongyouxia <gzcl_wcon@126.com>
2018-01-17 15:00:19 +0800

    评论分类
    评论分类

---
+++ TensorExpand/项目练习/8、tensorflow对评论进行分类/README.md
@@ -0,0 +1,54 @@
+参考：http://blog.topspeedsnail.com/archives/10399
+
+- TensorFlow文档：http://tensorflow.org
+- [使用Python实现神经网络](http://blog.topspeedsnail.com/archives/10377)
+- [Ubuntu 16.04 安装 Tensorflow(GPU支持)](http://blog.topspeedsnail.com/archives/10116)
+- Andrew Ng斯坦福公开课
+- https://github.com/deepmind
+
+
+----------
+本帖展示怎么使用TensorFlow实现文本的简单分类，判断评论是正面的还是负面的。
+
+# 使用的数据集
+
+英文的[电影评论数据](https://www.cs.cornell.edu/people/pabo/movie-review-data/)（其实不管是英文还是中文，处理逻辑都一样）。
+
+![这里写图片描述](http://blog.topspeedsnail.com/wp-content/uploads/2016/11/%E5%B1%8F%E5%B9%95%E5%BF%AB%E7%85%A7-2016-11-13-%E4%B8%8B%E5%8D%889.36.26.png)
+
+neg.txt：5331条负面电影评论(http://blog.topspeedsnail.com/wp-content/uploads/2016/11/neg.txt) （每一行对应一条评论）
+
+pos.txt：5331条正面电影评论 (http://blog.topspeedsnail.com/wp-content/uploads/2016/11/pos.txt)（每一行对应一条评论）
+
+由于处理的是字符串，我们首先要想方法把字符串转换为向量/数字表示。一种解决方法是可以把单词映射为数字ID。
+
+第二个问题是每行评论字数不同，而神经网络需要一致的输入(其实有些神经网络不需要，至少本帖需要)，这可以使用词汇表解决。
+
+# 代码部分
+安装nltk（自然语言工具库 [Natural Language Toolkit](http://www.nltk.org/)）
+
+```python
+pip install nltk
+```
+下载nltk数据：
+
+```python
+$ python
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 26 2016, 10:47:25) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import nltk
+>>> # nltk.download()
+>>> nltk.download('punkt')
+>>> nltk.download('wordnet')
+```
+测试nltk安装：
+
+```python
+>>> from nltk.corpus import brown
+>>> brown.words()
+['The', 'Fulton', 'County', 'Grand', 'Jury', 'said', ...]
+```
+
+# 思路流程
+![这里写图片描述](http://img.blog.csdn.net/20180117145427716?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvd2M3ODE3MDgyNDk=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast)
---
+++ TensorExpand/项目练习/8、tensorflow对评论进行分类/main.py
@@ -0,0 +1,219 @@
+# python3
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import slim,rnn
+import random
+import pickle
+from collections import Counter
+
+import nltk
+from nltk.tokenize import word_tokenize
+
+"""
+'I'm super man'
+tokenize:
+['I', ''m', 'super','man' ] 
+"""
+from nltk.stem import WordNetLemmatizer
+
+"""
+词形还原(lemmatizer)，即把一个任何形式的英语单词还原到一般形式，与词根还原不同(stemmer)，后者是抽取一个单词的词根。
+"""
+
+pos_file = 'pos.txt'
+neg_file = 'neg.txt'
+
+
+# 创建词汇表
+def create_lexicon(pos_file, neg_file):
+    lex = []
+
+    # 读取文件
+    def process_file(pos_file):
+        with open(pos_file, 'r') as f:
+            lex = []
+            lines = f.readlines() # 读取全部行
+            # print(lines)
+            for line in lines: # 每一行对应一条评论
+                words = word_tokenize(line.lower()) # 每一行拆成一个个单词（不是字母）
+                lex += words # 将所有单词 放在列表中 （包括重复的）
+            return lex
+
+    lex += process_file(pos_file)
+    lex += process_file(neg_file)
+    # print(len(lex))
+    lemmatizer = WordNetLemmatizer()
+    lex = [lemmatizer.lemmatize(word) for word in lex]  # 词形还原 (cats->cat) 过去时 进行时 等还原成一般形式
+
+    word_count = Counter(lex) # 统计每个词出现的频率 返回类似一个字典（起到去重） 转成字典 dict(word_count.items())
+    # print(word_count)
+    # {'.': 13944, ',': 10536, 'the': 10120, 'a': 9444, 'and': 7108, 'of': 6624, 'it': 4748, 'to': 3940......}
+    # 去掉一些常用词,像the,a and等等，和一些不常用词; 这些词对判断一个评论是正面还是负面没有做任何贡献
+    lex = []
+    for word in word_count:
+        if word_count[word] < 2000 and word_count[word] > 20:  # 这写死了，好像能用百分比  选取词出现的频率 20~2000之间的词
+            lex.append(word)  # 齐普夫定律-使用Python验证文本的Zipf分布 http://blog.topspeedsnail.com/archives/9546
+    return lex
+
+
+lex = create_lexicon(pos_file, neg_file) # 词汇表
+
+
+# lex里保存了文本中出现过的单词。
+
+# 把每条评论转换为向量, 转换原理：
+# 假设lex为['woman', 'great', 'feel', 'actually', 'looking', 'latest', 'seen', 'is'] 当然实际上要大的多
+# 评论'i think this movie is great' 转换为 [0,1,0,0,0,0,0,1], 把评论中出现的字在lex中标记，出现过的标记为1，其余标记为0
+def normalize_dataset(lex):
+    dataset = []
+
+    # lex:词汇表；review:评论；clf:评论对应的分类，[0,1]代表负面评论 [1,0]代表正面评论
+    def string_to_vector(lex, review, clf):
+        words = word_tokenize(review.lower())
+        lemmatizer = WordNetLemmatizer()
+        words = [lemmatizer.lemmatize(word) for word in words]
+
+        features = np.zeros(len(lex)) # 取词汇表的长度（保证所有特征长度相等） 而不是取每条评论的长度（每条评论长度不一致）
+        for word in words:
+            if word in lex:
+                features[lex.index(word)] = 1  # 一个句子中某个词可能出现两次,可以用+=1，其实区别不大
+        return [features, clf]
+
+    with open(pos_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            one_sample = string_to_vector(lex, line, [1, 0])  # [array([ 0.,  1.,  0., ...,  0.,  0.,  0.]), [1,0]]
+            dataset.append(one_sample)
+    with open(neg_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            one_sample = string_to_vector(lex, line, [0, 1])  # [array([ 0.,  0.,  0., ...,  0.,  0.,  0.]), [0,1]]]
+            dataset.append(one_sample)
+
+    # print(len(dataset))
+    return dataset
+
+
+dataset = normalize_dataset(lex)
+random.shuffle(dataset)
+"""
+#把整理好的数据保存到文件，方便使用。到此完成了数据的整理工作
+with open('save.pickle', 'wb') as f:
+	pickle.dump(dataset, f)
+"""
+
+# 取样本中的10%做为测试数据
+test_size = int(len(dataset) * 0.1)
+
+dataset = np.array(dataset) # [10662,2]
+
+train_dataset = dataset[:-test_size] # [9596,2]  train_dataset[0][0] 对应评论数据 ；train_dataset[0][1] 对应评论数据的标签
+test_dataset = dataset[-test_size:] #
+
+# Feed-Forward Neural Network
+# 定义每个层有多少'神经元''
+n_input_layer = len(lex)  # 输入层 词汇表尺寸，也是每条评论对应的向量长度
+
+n_layer_1 = 100 #1000  # hide layer
+n_layer_2 = 1000  # hide layer(隐藏层)听着很神秘，其实就是除输入输出层外的中间层
+
+n_output_layer = 2  # 输出层
+
+
+# 定义待训练的神经网络（DNN）
+def neural_network(data):
+    # 定义第一层"神经元"的权重和biases
+    layer_1_w_b = {'w_': tf.Variable(tf.random_normal([n_input_layer, n_layer_1])),
+                   'b_': tf.Variable(tf.random_normal([n_layer_1]))}
+    # 定义第二层"神经元"的权重和biases
+    layer_2_w_b = {'w_': tf.Variable(tf.random_normal([n_layer_1, n_layer_2])),
+                   'b_': tf.Variable(tf.random_normal([n_layer_2]))}
+    # 定义输出层"神经元"的权重和biases
+    layer_output_w_b = {'w_': tf.Variable(tf.random_normal([n_layer_2, n_output_layer])),
+                        'b_': tf.Variable(tf.random_normal([n_output_layer]))}
+
+    # w·x+b
+    layer_1 = tf.add(tf.matmul(data, layer_1_w_b['w_']), layer_1_w_b['b_'])
+    layer_1 = tf.nn.relu(layer_1)  # 激活函数
+    layer_2 = tf.add(tf.matmul(layer_1, layer_2_w_b['w_']), layer_2_w_b['b_'])
+    layer_2 = tf.nn.relu(layer_2)  # 激活函数
+    layer_output = tf.add(tf.matmul(layer_2, layer_output_w_b['w_']), layer_output_w_b['b_'])
+
+    return layer_output
+
+# rnn 网络
+def rnn_net(x):
+    # 将x 从二维转成3维 目的使用rnn（输入需是3维数据）
+    inputs=slim.embed_sequence(tf.cast(x,tf.int32),vocab_size=len(lex),embed_dim=n_layer_1) # [none,len(train_dataset[0][0]),n_layer_1] 3维
+    cell = tf.contrib.rnn.BasicLSTMCell(n_layer_1, state_is_tuple=True)
+    cell = tf.nn.rnn_cell.MultiRNNCell([cell] * 4, state_is_tuple=True) # 多层核
+    initial_state = cell.zero_state(batch_size, tf.float32)
+    outputs, last_state = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state, scope='rnnlm')
+    # outputs shape [batch_size,len(train_dataset[0][0]),n_layer_1]
+
+    results=slim.fully_connected(outputs[:,-1,:],n_output_layer,activation_fn=None) # [batch,2]
+
+    return results
+
+# 每次使用50条数据进行训练
+batch_size = 16 #50
+
+X = tf.placeholder('float', [None, len(train_dataset[0][0])])
+# [None, len(train_x)]代表数据数据的高和宽（矩阵），好处是如果数据不符合宽高，tensorflow会报错，不指定也可以。
+Y = tf.placeholder('float') # Y = tf.placeholder('float',[None,2])
+
+
+# 使用数据训练神经网络
+def train_neural_network(X, Y):
+    # predict = neural_network(X)
+    predict = rnn_net(X)
+
+    cost_func = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predict, labels=Y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost_func)  # learning rate 默认 0.001
+
+    epochs = 13
+    with tf.Session() as session:
+        session.run(tf.global_variables_initializer())
+        epoch_loss = 0
+
+        # i = 0
+        random.shuffle(train_dataset)
+        train_x = dataset[:, 0]
+        train_y = dataset[:, 1]
+
+        steps=len(train_x)//batch_size
+
+        for epoch in range(epochs):
+            start=0;end=0
+            for step in range(steps):
+                end=start+batch_size
+                batch_x = train_x[start:end]
+                batch_y = train_y[start:end]
+                _, c = session.run([optimizer, cost_func], feed_dict={X: list(batch_x), Y: list(batch_y)})
+                epoch_loss += c
+
+                start=end
+
+            '''       
+            while i < len(train_x):
+                start = i
+                end = i + batch_size
+
+                batch_x = train_x[start:end]
+                batch_y = train_y[start:end]
+
+                _, c = session.run([optimizer, cost_func], feed_dict={X: list(batch_x), Y: list(batch_y)})
+                epoch_loss += c
+                i += batch_size
+            '''
+            print(epoch, ' : ', epoch_loss/steps)
+
+        text_x = test_dataset[:, 0]
+        text_y = test_dataset[:, 1]
+        correct = tf.equal(tf.argmax(predict, 1), tf.argmax(Y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('准确率: ', accuracy.eval({X: list(text_x), Y: list(text_y)}))
+
+
+train_neural_network(X, Y)
