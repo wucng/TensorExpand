d8ab4bdcf09f52b71b01cfbc703b612bc94d722f
fengzhongyouxia <gzcl_wcon@126.com>
2018-03-18 23:48:45 +0800

    MSCOCO

--- TensorExpand/Object detection/Data_interface/MSCOCO/one_image_json.py
+++ TensorExpand/Object detection/Data_interface/MSCOCO/one_image_json.py
@@ -18,8 +18,8 @@ import pylab
 import json
 
 json_file='./annotations/instances_val2017.json' # # Object Instance 类型的标注
-# person_keypoints_val2017.json  # Object Keypoint 类型的标注格式
-# captions_val2017.json  # Image Caption的标注格式
+# json_file='./annotations/person_keypoints_val2017.json'  # Object Keypoint 类型的标注格式
+# json_file='./annotations/captions_val2017.json' # Image Caption的标注格式
 
 data=json.load(open(json_file,'r'))
 
@@ -27,7 +27,7 @@ data_2={}
 data_2['info']=data['info']
 data_2['licenses']=data['licenses']
 data_2['images']=[data['images'][0]] # 只提取第一张图片
-data_2['categories']=data['categories']
+data_2['categories']=data['categories']  # Image Caption 没有该字段
 annotation=[]
 
 # 通过imgID 找到其所有对象
@@ -40,3 +40,5 @@ data_2['annotations']=annotation
 
 # 保存到新的JSON文件，便于查看数据特点
 json.dump(data_2,open('./new_instances_val2017.json','w'),indent=4) # indent=4 更加美观显示
+# json.dump(data_2,open('./new_person_keypoints_val2017.json','w'),indent=4) # indent=4 更加美观显示
+# json.dump(data_2,open('./new_captions_val2017.json','w'),indent=4) # indent=4 更加美观显示
